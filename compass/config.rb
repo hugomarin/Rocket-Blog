0507c85789ef6dadbb254e99bbd83fa64a9588dc
hugomarin <hm@hugomarin.co>
2013-05-03 15:30:48 -0500

    first commit

---
+++ compass/config.rb
@@ -0,0 +1,23 @@
+# Compass configuration file.
+
+# We also support plugins and frameworks, please read the docs http://docs.mixture.io/preprocessors#compass
+project_path = "/Users/Hugo/Dropbox/Mixture/Rocket Blog/"
+
+# Important! change the paths below to match your project setup
+
+css_dir = "assets/css" # update to the path of your css files.
+
+sass_dir = "assets/scss" # update to the path of your sass files.
+
+images_dir = "assets/img" # update to the path of your image files.
+
+javascripts_dir = "assets/scripts" # update to the path of your script files.
+
+line_comments = false
+
+cache = true
+
+color_output = false # required for Mixture
+
+require 'modular-scale'
+
